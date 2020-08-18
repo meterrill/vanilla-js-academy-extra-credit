4722fb152ec25b87d58b345d6df96b567be3d09c
Matthew Terrill <meterrill@gmail.com>
2020-08-17 19:21:53 -0700

    Create an object to map the word count (d02)

--- 02-word-count-frequency/script.js
+++ 02-word-count-frequency/script.js
@@ -4,15 +4,27 @@ var wordCount = document.querySelector('#word-count');
 var characterCount = document.querySelector('#character-count');
 
 // Listen for input on the textarea
-text.addEventListener('input', function () {
+text.addEventListener('input', function() {
   // Create an array of strings from the textarea
-  var stringsArray = text.value.split(/\s+/);
+  var strings = text.value.split(/\s+/);
+  
   // Filter empty values from the array of strings
-  var wordsArray = stringsArray.filter(function (string) {
+  var words = strings.filter(function(string) {
     return string.length > 0;
   });
+
+  // Create an object to map the word count
+  var wordsMap = {};
+  words.forEach(function(word) {
+    if (wordsMap.hasOwnProperty(word)) {
+      wordsMap[word]++;
+    } else {
+      wordsMap[word] = 1;
+    }
+  });
+
   // Update the word count in the DOM
-  wordCount.textContent = wordsArray.length;
+  wordCount.textContent = words.length;
 
   // Get the length of the value in the textarea and update the DOM
   characterCount.textContent = text.value.length;
