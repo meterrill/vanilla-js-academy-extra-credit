e7a079801739887f1962f3449260915a62f3e9b9
Matthew Terrill <meterrill@gmail.com>
2020-08-18 15:56:48 -0700

    Refactor (d02(

--- 02-word-count-frequency/script.js
+++ 02-word-count-frequency/script.js
@@ -1,47 +1,98 @@
-// Get textarea, span elements, and #word-list div
+// Get the textarea
 var text = document.querySelector('#text');
+
+// Get the #word-count element
 var wordCount = document.querySelector('#word-count');
+
+// Get the #character-count element
 var characterCount = document.querySelector('#character-count');
+
+// Get the #word-list ul
 var wordList = document.querySelector('#word-list');
 
-// Listen for input on the textarea
-text.addEventListener('input', function() {
-  // Create an array of strings from the textarea
-  var strings = text.value.split(/\s+/);
+/**
+ * Get an array of words
+ * @param   {String} text The text value from the textarea
+ * @returns {Array}       The array of words
+ */
+function getWordsArray(text) {
+  // Create an array of words from the text string
+  var wordsArray = text.split(/\W+/);
   
-  // Filter empty values from the array of strings
-  var words = strings.filter(function(string) {
-    return string.length > 0;
+  // Filter empty values from the array of words
+  wordsArray = wordsArray.filter(function(word) {
+    return word.length > 0;
   });
+  
+  // Return the array of words
+  return wordsArray;
+}
 
+/**
+ * Get an object of words and their counts
+ * @param   {Array} wordsArray The array of words
+ * @returns {Object}           The object of words and their counts
+ */
+function getWordsMap(wordsArray) {
   // Create an object to map the word count
   var wordsMap = {};
-  words.forEach(function(word) {
+
+  // Count the words
+  wordsArray.forEach(function(word) {
+    // Convert the word to lower case
+    word = word.toLowerCase();
+
+    // If the word exists in the object
     if (wordsMap.hasOwnProperty(word)) {
+      // Increment the count
       wordsMap[word]++;
     } else {
+      // Add the word to the object and set the count to 1
       wordsMap[word] = 1;
     }
   });
 
-  // Sort the words in descending order
-  var sortedWords = Object.keys(wordsMap).map(function(word) {
+  // Return the object
+  return wordsMap;
+}
+
+/**
+ * Sort the list of words by their count
+ * @param   {Object} wordsMap The list of words and their counts
+ * @returns {Array}           The sorted array
+ */
+function sortWordsList(wordsMap) {
+  // Create an array of objects for each word
+  var wordsList = Object.keys(wordsMap).map(function(key) {
     return {
-      word: word,
-      total: wordsMap[word]
+      word: key,
+      count: wordsMap[key]
     };
-  }).sort(function(a, b) {
-    return b.total - a.total;
+  });
+  
+  // Sort the words in descending order
+  wordsList.sort(function(a, b) {
+    return b.count - a.count;
   });
 
-  // Update the word count in the DOM
-  wordCount.textContent = words.length;
+  // Return the sorted array
+  return wordsList;
+}
 
-  // Get the length of the value in the textarea and update the DOM
+// Listen for input on the textarea
+text.addEventListener('input', function() {
+  var wordsArray = getWordsArray(text.value);
+  var wordsMap = getWordsMap(wordsArray);
+  var sortedWords = sortWordsList(wordsMap);
+
+  // Add the word count to the DOM
+  wordCount.textContent = wordsArray.length;
+
+  // Add the character count to the DOM
   characterCount.textContent = text.value.length;
 
-  // Add the sorted words list to the DOM
+  // Add the words list to the DOM
   wordList.innerHTML = sortedWords.map(function(word) {
-    return `<li>${word.word}: ${word.total}</li>`;
+    return `<li>${word.word}: ${word.count}</li>`;
   }).join('');
 });
