efa7c3392439fa0351f3c2ed0887afcfbd49e6cc
Matthew Terrill <meterrill@gmail.com>
2020-08-19 22:01:40 -0700

    Add the GIFs to the DOM (d03)

--- 03-gif-search-app/script.js
+++ 03-gif-search-app/script.js
@@ -9,6 +9,26 @@ var apiKey = '6PMeaEBxQZUYtzenqPxie3dt4XyehQh3';
 var endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=`
 
 /**
+ * Add the GIFs to the DOM
+ * @param {Object} data The API data
+ */
+function showGIFs(data) {
+  searchResults.innerHTML = `
+    <ul>
+      ${data.data.map(function(gif) {
+        return `
+          <li>
+            <a href="${gif.url}">
+              <img src="${gif.images.downsized.url}" alt="${gif.title}">
+            </a>
+          </li>
+        `;
+      }).join('')}
+    </ul>
+  `;
+}
+
+/**
  * Get the data from the API
  * @param   {String} searchQuery The search query term or phrase
  * @returns {Object}             The API data
@@ -17,7 +37,7 @@ function getGIFs(searchQuery) {
   fetch(endpoint + searchQuery).then(function(response) {
     return response.ok ? response.json() : Promise.reject(response);
   }).then(function(data) {
-    console.log(data);
+    showGIFs(data);
   }).catch(function(error) {
     console.log(error);
   });
