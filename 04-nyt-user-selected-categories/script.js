c87845312db5ae7c8aa103b9a189882545587f13
Matthew Terrill <meterrill@gmail.com>
2020-08-22 09:32:32 -0700

    Revert layout (d04)

--- 04-nyt-user-selected-categories/script.js
+++ 04-nyt-user-selected-categories/script.js
@@ -1,43 +1,50 @@
 // Get the #app element
 var app = document.querySelector('#app');
 
-// Store the sections, endpoint, and API key
-var sections = ['world', 'us', 'sports', 'technology']
+// Store the endpoint and API key
 var endpoint = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=';
 var apiKey = '0q2Pwnu4SiIy7UxplCJ745E0PGRohgVb';
 
-// Fetch the stories from the API
-function getStories(section, endpoint) {
-  fetch(endpoint).then(function (response) {
-    return response.ok ? response.json() : Promise.reject(response);
-  }).then(function (data) {
-    app.innerHTML += '<section>' + 
-      '<h2>' + 
-      '<a href="https://www.nytimes.com/section/' + section + '">' + section + '</a>' + 
-      '</h2>' + 
-      '<div>' + 
-      data.results.slice(0, 3).map(function (story) {
-        return (
-          '<article>' +
-          '<a href="' + story.url + '">' +
-          '<img src="' + story.multimedia[0].url + '" alt="' + story.multimedia[0].caption + '">' +
-          '<h3>' + story.title + '</h3>' +
-          '<p>' + story.abstract + '</p>' +
-          '<time datetime="' + story.published_date + '">' + new Date(story.published_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) + '</time>' +
-          '</a>' +
-          '</article>'
-        );
-      }).join('') + 
-      '</div>' + 
-      '</section>';
-  }).catch(function (error) {
-    app.innerHTML = '<p>We\'re experiencing some technical difficulties. Please try again later or view today\'s top stories directly on <a href="https://www.nytimes.com/">The New York Times</a> website.</p>';
-  });
+/**
+ * Get the JSON from the API
+ * @param   {Object} response The response from the API
+ * @returns {Object}          The JSON data or a rejected promise
+ */
+function getJSON(response) {
+  return response.ok ? response.json() : Promise.reject(response);
 }
 
-// Get stories for each section
-sections.forEach(function (section) {
-  endpoint = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json?api-key=' + apiKey;
+/**
+ * Add the articles to the DOM
+ * @param   {Object} articles The JSON data from the API
+ */
+function displayArticles(articles) {
+  app.innerHTML = articles.results.map(function (article) {
+    return (
+      '<article>' +
+      '<h2>' + article.section + '</h2>' +
+      '<a href="' + article.url + '">' +
+      '<img src="' + article.multimedia[0].url + '" alt="' + article.multimedia[0].caption + '">' +
+      '<h3>' + article.title + '</h3>' +
+      '<p>' + article.abstract + '</p>' +
+      '<time datetime="' + article.published_date + '">' + new Date(article.published_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) + '</time>' +
+      '</a>' +
+      '</article>'
+    );
+  }).join('');
+}
 
-  getStories(section, endpoint);
-});
+/**
+ * Add an error message to the DOM
+ * @param   {Object}  error The rejected promise
+ */
+function displayError(error) {
+  app.style.display = 'block';
+  app.innerHTML = '<p class="error">We\'re experiencing some technical difficulties.<br>Please try again later or view today\'s top stories directly on <a href="https://www.nytimes.com/">The New York Times</a> website.</p>';
+}
+
+// Fetch the articles from the API and add them to the DOM
+fetch(endpoint + apiKey)
+  .then(getJSON)
+  .then(displayArticles)
+  .catch(displayError);
