84f08889151749e4ca63750e02f381fe0693dfce
Matthew Terrill <meterrill@gmail.com>
2020-08-26 14:03:53 -0700

    Update to include all heading elements (d06)

--- 06-table-of-contents-multiple-heading-levels/script.js
+++ 06-table-of-contents-multiple-heading-levels/script.js
@@ -1,34 +1,52 @@
 ;(function () {
-	// Get the  #table-of-contents element
+
+	// Get the #table-of-contents element
 	var tableOfContents = document.querySelector('#table-of-contents');
 	
-	// Get the h2 elements
-	var headings = document.querySelectorAll('h2');
+	// Get the heading elements
+  var headings = document.querySelectorAll('h2, h3, h4, h5, h6');
 	
-	// Create ID from heading text
+	/**
+   * Create an ID from the heading text
+   * @param   {Node}  heading  The current heading
+   * @returns {String}         The ID
+   */
 	function createID(heading) {
-		heading.id = heading.textContent.replace(/\W+/g, '-').toLowerCase();
-	}
+    heading.id = heading.textContent.replace(/\W+/g, '-').toLowerCase();
+  }
 	
-	// Create a list item/link from heading
+	/**
+   * Create a list item/anchor from heading
+   * @param   {Node}  heading The current heading
+   * @returns {String}        The list item/anchor
+   */
 	function createLink(heading) {
 		return `<li>
 			<a href="#${heading.id}">${heading.innerText}</a>
 		</li>`;
-	}
-	
-	// Only generate markup if h2 elements exist
-	if (headings.length > 0) {
-		// Create a list of anchor links and render to the DOM
-		tableOfContents.innerHTML = `<ul>
-			${Array.prototype.slice.call(headings).map(function(heading) {
-				// Check for missing IDs
-				if (!heading.id) {
-					createID(heading);
-				}
-	
-				return createLink(heading);
-			}).join('')}
-		</ul>`;
-	}
+  }
+  
+  /**
+   * Display the Table of Contents
+   */
+  function generateTOC() {
+    // Only generate markup if heading elements exist
+    if (headings.length > 0) {
+      // Create a list of anchor links and render to the DOM
+      tableOfContents.innerHTML = `<ul>
+        ${Array.prototype.slice.call(headings).map(function(heading) {
+          // Check for missing IDs
+          if (!heading.id) {
+            createID(heading);
+          }
+    
+          return createLink(heading);
+        }).join('')}
+      </ul>`;
+    }
+  }
+  
+  // Display the Table of Contents
+  generateTOC();
+  
 })();
